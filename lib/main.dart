699fcd7327e5ccac0f984b445b41184dcc1ea0f4
AryaPramudya898 <aryapramudya610@gmail.com>
2026-02-27 05:53:10 +0700

    hilangin debug sama ganti warna seed

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purpleAccent),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
