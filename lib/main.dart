ff50daf69cd933572b7076b1f3be5b2a7884cf2d
AryaPramudya898 <aryapramudya610@gmail.com>
2026-02-27 05:36:21 +0700

    hapus variabel increment

--- lib/main.dart
+++ lib/main.dart
@@ -40,13 +40,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -68,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: ,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
